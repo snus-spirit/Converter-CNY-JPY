ea4986b7df98092ee602488072fd41a9f88012cd
maksim <macum001@mail.ru>
2023-02-16 21:00:06 +0300

    #1 Add converter CNY to JPY

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static void convertCNYtoJPY(double value) {
+        System.out.printf("%.2f CNY = %.2f JPY\n\n", value, value * getCurrencyRate());
+    }
+
+    private static double getCurrencyRate() {
+        Double rate = null;
+        try {
+            JsonNode json = new ObjectMapper().readTree(new URL("https://api.exchangerate.host/latest?base=CNY"));
+            rate = json.get("rates").get("JPY").asDouble();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return rate;
+    }
+}
